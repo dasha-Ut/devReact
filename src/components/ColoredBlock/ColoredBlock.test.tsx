d5bc0bdc5589d6241d1926e6c4d04ca89b1246a7
Daryna Utkina <dashaelbereth1@gmail.com>
2025-02-15 23:08:29 +0200

    added unit test for ColoredBlock

---
+++ src/components/ColoredBlock/ColoredBlock.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ChapterWrapper } from 'components/ChapterWrapper/ChapterWrapper';
+import { useRerender } from 'hooks/useRerender';
+import { BrowserRouter } from 'react-router-dom';
+import { ColoredBlock } from './ColoredBlock';
+
+function WithChapterWrapper() {
+    const rerender = useRerender();
+    return (
+        <BrowserRouter>
+            <ChapterWrapper title="Chapter title" rerender={rerender}>
+                <ColoredBlock>Inside the chapter wrapper</ColoredBlock>
+            </ChapterWrapper>
+        </BrowserRouter>
+    );
+}
+
+describe('Colored block render', () => {
+    test('should render correctly', async () => {
+        const content = 'Colored block content';
+        render(<ColoredBlock>{content}</ColoredBlock>);
+
+        expect(screen.getByText(content)).toBeVisible();
+    });
+
+    test('should override default styles', async () => {
+        const content = 'Colored block content';
+        render(<ColoredBlock style={{ display: 'none' }}>{content}</ColoredBlock>);
+
+        expect(screen.getByText(content)).not.toBeVisible();
+    });
+
+    test('should change color each render', async () => {
+        render(<WithChapterWrapper />);
+
+        expect(screen.getByText('🔄')).toBeVisible();
+        const bgColor = screen.getByTestId('colored-block').style.backgroundColor;
+
+        await userEvent.click(screen.getByText('🔄'));
+
+        const newBgColor = screen.getByTestId('colored-block').style.backgroundColor;
+        expect(newBgColor).not.toBe(bgColor);
+    });
+})
